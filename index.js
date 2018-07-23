1325304457c84566448aec28a63eb7f029904e33
Carl Steele <casteeleu@gmail.com>
2018-07-23 22:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+
+const newID = (1000000000 + oldID); 
+
+// we assume we have a variable called currentAge. the test will set this var
+ // write some code here that is true if currentAge is a number
+ 
+const ageIsValid = (currentAgeIsInteger);
+
+Number.isInteger()
+
+Math.random()
+Number.isInteger(randomUserID); 
+
+
+
+
